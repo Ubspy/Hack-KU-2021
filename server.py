43fad4b2a762a26c03ebb167b3a5b03c498cd821
Ubspy <jack-m@live.com>
2021-04-10 15:50:25 -0500

    Fixed casing, added blockchain implementation for signed medical changes, added some server back end post handling templates

--- server.py
+++ server.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/authPatient')
+def parientView():
+    return None
+
+@app.route('/authDoctor')
+def doctorView():
+    return None
+
+@app.route('/requestPatientEdits', method=['POST'])
+def writePatientInfo():
+    

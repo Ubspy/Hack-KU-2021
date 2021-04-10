5c37e1f9cd6b7504f0d387acc5b996bc4079a79f
Andrew Riachi <brasswood@users.noreply.github.com>
2021-04-10 14:37:32 -0500

    test medical change

--- testSampleMedicalChange.py
+++ testSampleMedicalChange.py
@@ -1,5 +1,34 @@
-from medicalChange import load
+from medicalChange import *
+from medicalData import *
+from datetime import datetime
+import json
+from pprint import pprint
+from cryptography.hazmat.primitives import serialization
 
-with open("samplemedicalchange.json") as f:
-    load(f)
-    
+mychange = MedicalChange(
+    "measurements", 
+    PatientMeasurements(
+        Date(4, 10, 2021), 
+        171,
+        None, 
+        None
+    ),
+    datetime.now()
+)
+
+with open("key.pem", "rb") as private_key_file:
+    private_key = serialization.load_pem_private_key(
+        private_key_file.read(),
+        password=b"passphrase"
+    )
+signedchange = sign(mychange, private_key)
+
+encoded = json.dumps(signedchange, cls=MedicalChangeEncoder)
+print(encoded)
+
+with open("public.pem", "rb") as public_key_file:
+    public_key = serialization.load_pem_public_key(
+        public_key_file.read()
+    )
+
+verify(signedchange, public_key)
